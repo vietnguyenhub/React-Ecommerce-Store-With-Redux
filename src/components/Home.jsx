d17fb1c4c5863f2b415aff6acabebee3553aa6eb
vietnguyxn <vietnguyenhht@gmail.com>
2022-03-16 12:52:22 +0700

    Emart

---
+++ src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Products } from "./Products";
+
+export const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card bg-dark text-white border-0">
+        <img
+          src="/assets/bg.jpg"
+          className="card-img"
+          alt="background"
+          height="auto"
+        />
+        <div className="card-img-overlay d-flex justify-content-end">
+          <div className="container" style={{ textAlign: "right" }}>
+            <h5 className="card-title display-3 fw-border mb-2">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </div>
+  );
+};
